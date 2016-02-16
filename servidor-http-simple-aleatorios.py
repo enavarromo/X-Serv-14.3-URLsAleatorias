ca02c28acda95064380ab657554850e6f14aa724
Eloy Navarro Morales <enavarro@zeta40.aulas.gsyc.es>
2016-02-16 11:00:24 +0100

    Comentarios para sumar

--- servidor-http-simple-aleatorios.py
+++ servidor-http-simple-aleatorios.py
@@ -21,6 +21,12 @@ try:
         # -------------- Plot Socket Info --------------
         print 'HTTP request received from: '+ str(address)
         print recvSocket.recv(1024)
+# Cargar la recepcion, split para generar lista, split() separa espacios.
+# Coger segundo elemento sin la barra: rx.split()[1][1:]
+# Una vez aislado, guardar cifra y pedir la segunda para sumar...
+# Una maquina de dos estados para almacenar y luego sumar, esto asume que el que
+# envia el primero y el segundo numero son el mismo tipo, si hay uan peticion
+# entre medias, se jorobo el asunto. Como si no demomento.
         if address[0] == '127.0.0.1':
             print "Coming from Loopback"
             # -------------- HTTP to LoopBack --------------
@@ -28,8 +34,9 @@ try:
                             "<html><body><h1>Hello World!</h1>" +
                             "You came from LoopBack at IP: " +
                             str(address[0]) + 
-                            " and Port: " + str(address[1]) +
-                            " Your random is: " + str(random.randint) +
+                            " and Port: " +  str(address[1]) + "</p>" +
+                            "<a href=http://"+host+":"+str(port)+"/"+
+                            str(random.randint(1, 1000))+">enlace</a>"
                             "</body></html>\r\n")
         else:
             # -------------- HTTP to ext --------------
